95db9b3562e4af57659fac0d84a9a2961f07de5c
Yikoutian1 <qiulihang1@foxmail.com>
2023-12-10 19:04:53 +0800

    feat(工资信息): 工资信息

--- salary-manage/src/main/java/com/hang/controller/SalaryInfoController.java
+++ salary-manage/src/main/java/com/hang/controller/SalaryInfoController.java
@@ -1,4 +1,9 @@
 package com.hang.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/salaryInfo")
 public class SalaryInfoController {
 }
